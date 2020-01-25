b52b2dce744e42779b4469efd830d8a9719c83e2
Travis Tressler <r.travis.tressler@gmail.com>
2020-01-25 01:23:38 -0500

    unit testing scaffold is complete

---
+++ test/weather_bloc_test.dart
@@ -0,0 +1,48 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+
+void main() {
+
+  group('modifying saved locations', () {
+    test('adding new place successfully', () {
+
+    });
+
+    test('deleting place successfully', () {
+
+    });
+
+    test('stopping user from adding more than 5 places successfully', () {
+
+    });
+
+    test('stopping user from adding a duplicate location successfully', () {
+
+    });
+  });
+
+  group('search', () {
+    test('not finding searched location', () {
+
+    });
+
+    test('searching for blank location', () {
+
+    });
+  });
+
+  group('save functionality', () {
+    test('getting 5 local forecasts if there are 5 items in shared preferences on first load', () {
+
+    });
+
+    test('show no locations text on launch if shared pref size is 0', () {
+
+    });
+
+    test('show no locations text on removing of location if shared pref size is 0', () {
+
+    });
+  });
+}
