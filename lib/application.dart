adb60a979c4f2439d1594d108784710f354429f7
Travis Tressler <r.travis.tressler@gmail.com>
2020-01-24 21:30:21 -0500

    modified splash home route to inject into it a bloc

--- lib/application.dart
+++ lib/application.dart
@@ -2,6 +2,7 @@
 import 'package:flutter/material.dart';
 import 'package:generic_bloc_provider/generic_bloc_provider.dart';
 import 'package:weather_oauth/blocs/login_bloc.dart';
+import 'package:weather_oauth/blocs/splash_bloc.dart';
 import 'package:weather_oauth/routing/login_route.dart';
 import 'package:weather_oauth/routing/weather_route.dart';
 import 'package:weather_oauth/screens/login_screen.dart';
@@ -12,6 +13,7 @@ import 'package:weather_oauth/services/repository_impl.dart';
 
 class Application extends StatelessWidget {
 
+  ///since this is a small app, we're going to use one instance of our repo class, since Firebase is such an expensive object
   final Repository _repository = RepositoryImpl();
 
   @override
@@ -19,7 +21,10 @@ class Application extends StatelessWidget {
     return MaterialApp(
       title: 'Weather OAuth',
       debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
+      home: BlocProvider(
+          bloc: SplashBloc(_repository),
+          child: SplashScreen()
+      ),
       routes: {
         LoginRoute.routeName : (context) => BlocProvider(
           bloc: LoginBloc(_repository),
